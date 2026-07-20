b6a1eb8aa28e7b087b5fd84e865a9eee25779f0f
Benjamin Blanchard <bblanchard.tech@gmail.com>
2026-07-20 15:49:02 -0400

    Update create-users.ps1

--- create-users.ps1
+++ create-users.ps1
@@ -1,10 +1,34 @@
-# 1. Définition du chemin automatique vers le fichier CSV
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+Import-Module ActiveDirectory
+
 $cheminFichier = "$PSScriptRoot\utilisateurs.csv"
 
-# 2. Importation des données des utilisateurs
-$listeUtilisateurs = Import-Csv -Path $cheminFichier
+$listeUtilisateurs = Import-Csv -Path $cheminFichier -Encoding utf8
 
-# 3. Boucle de traitement et validation de la lecture des données
+$motDePasseBrut = "Bienvenue123!"
+
+$motDePasseSecurise = ConvertTo-SecureString $motDePasseBrut -AsPlainText -Force
+
 foreach ($user in $listeUtilisateurs) {
-    Write-Host "Utilisateur trouvé : $($user.Prenom) qui travaille au département : $($user.Departement)"
+    $OU = "OU=$($user.Departement),DC=bblanchard,DC=lab"
+    
+    if (-not (Get-ADOrganizationalUnit -Filter "Name -eq '$($user.Departement)'")) {
+        Write-Host "Création de l'OU : $($user.Departement)" -ForegroundColor Yellow
+        New-ADOrganizationalUnit -Name $($user.Departement) -Path "DC=bblanchard,DC=lab"
+    }
+
+    if (-not (Get-ADUser -Filter "SamAccountName -eq '$($user.Username)'")) {
+        Write-Host "Création de l'utilisateur : $($user.Prenom) $($user.Nom)" -ForegroundColor Green
+        
+        New-ADUser -Name "$($user.Prenom) $($user.Nom)" `
+                   -SamAccountName $user.Username `
+                   -UserPrincipalName "$($user.Username)@bblanchard.lab" `
+                   -Path $OU `
+                   -AccountPassword $motDePasseSecurise `
+                   -ChangePasswordAtLogon $true `
+                   -Enabled $true
+    } else {
+        Write-Host "L'utilisateur $($user.Username) existe déjà. Passage au suivant." -ForegroundColor Cyan
+    }
 }
